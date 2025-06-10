101e54fa2f76898542e1fc0c9167b8bdf86c9e51
vivien <vivien@m2i.fr>
2025-06-10 12:22:07 +0200

    1er exo JEE c'était marrant

---
+++ Exo1JEE/Exo1JEE/src/main/java/org/example/exo1jee/Exo1.java
@@ -0,0 +1,66 @@
+package org.example.exo1jee;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "exo1serv", value = "/exo1serv")
+public class Exo1 extends HttpServlet {
+
+    private String texte;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Ma serviette est lancée ! (message dans la methode init)");
+        texte = "salut salut";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("une requete GET a ete envoyée a ma serviette !");
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>BONJOUR,JE SUIS NICOLAS SARKOZY ET J'AI LE PLAISIR DE LIRE : LE TEMPS DES TEMPETES POUR AUDIBLE</h1>");
+
+        // reponse HTML
+        resp.setContentType("text/html");
+
+        out.println("<html><body>");
+        out.println("<h1>"+texte+"</h1>");
+        out.println("<h2>TITRE LAAAAA</h2>");
+        out.println("<h3>ON FAIT DU HTML OU BIEN ??? TAVU</h3>");
+        out.println("</body></html>");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Exo1JEE/Exo1JEE/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<br/>
+<a href="txtBrut">texte brut ici</a>
+<a href="exo1serv">text html ici</a>
+<a href="exo1jsp">la fameuse page JeSaisPas</a>
+</body>
+</html>
